a94979aa5153fa9f133b5aba85e78ce458f91561
Paolo Sebastianelli <64946648+PaoloSebas@users.noreply.github.com>
2021-04-17 09:51:42 +1000

    Update and rename code1.R to Movielens_Machine_Learning_Project.R

---
+++ Movielens_Machine_Learning_Project.R
@@ -0,0 +1,17 @@
+### 
+###
+### title: "Movielens_Project"
+#### author: "Paolo Sebastianelli"
+#### cdate: "17/04/2021"
+output: to determine
+---
+
+
+## Project Overview: MovieLens
+
+# The purpouse of this project is to create a movie recommendation system (hereafter MVS) using the MovieLens dataset (hereafter MLD). Since the entire MLD contains milions of ratings, a subset will be used to show the results obtained by the MVS application. 
+
+# Generating the Data Set 
+
+# As first step the Data Set will be obtained by using this code:
+
